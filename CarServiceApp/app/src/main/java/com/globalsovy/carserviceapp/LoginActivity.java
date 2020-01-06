d55d289d5d2cd36f6a4f4b6988eed56aff47f1b0
Tomas <tomas.pavlik@akademiasovy.sk>
2020-01-06 17:08:37 +0100

    oujea

--- CarServiceApp/app/src/main/java/com/globalsovy/carserviceapp/LoginActivity.java
+++ CarServiceApp/app/src/main/java/com/globalsovy/carserviceapp/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.globalsovy.carserviceapp;
 
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableString;
+import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     TextView emailLabel;
     EditText passwordInp;
     TextView passwordLabel;
+    Button loginBtn;
+
+    String email="";
+    String password="";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +44,9 @@ public class LoginActivity extends AppCompatActivity {
         emailLabel = findViewById(R.id.labelEmail);
         passwordInp = findViewById(R.id.passwordEditTextInt);
         passwordLabel = findViewById(R.id.labelPassword);
+        loginBtn = findViewById(R.id.logInButton);
+
+        loginBtn.setEnabled(false);
 
         emailInp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -59,7 +73,8 @@ public class LoginActivity extends AppCompatActivity {
                     passwordInp.setTextColor(getResources().getColor(R.color.black));
                     passwordLabel.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                     passwordLabel.setTextColor(getResources().getColor(R.color.white));
-                }else{
+                }
+                else{
                     passwordInp.setBackground(getResources().getDrawable(R.drawable.fill_hard));
                     passwordInp.setTextColor(getResources().getColor(R.color.white));
                     passwordLabel.setBackgroundColor(getResources().getColor(R.color.white));
@@ -68,6 +83,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Login Reqeust sent");
+            }
+        });
     }
 
     public void setLogIn_toWhite(){
@@ -77,4 +98,41 @@ public class LoginActivity extends AppCompatActivity {
         spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.white)), text.length()-6, text.length(), 0);
         welcomeBack.setText(spannable);
     }
+
+    public void enable_disableLoginBTN(){
+        email = emailInp.getText().toString();
+        password = passwordInp.getText().toString();
+        if (password.length() >0 && email.length()>0) {
+            loginBtn.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            loginBtn.setTextColor(getResources().getColor(R.color.white));
+            loginBtn.setEnabled(true);
+        }else {
+            loginBtn.setBackgroundColor(getResources().getColor(R.color.buttonLoginColor));
+            loginBtn.setTextColor(getResources().getColor(R.color.buttonLoginColor));
+            loginBtn.setEnabled(false);
+        }
+    }
+    public void showToast(String text){
+        Toast.makeText(LoginActivity.this,text,Toast.LENGTH_SHORT).show();
+
+    }
+
 }
+
+
+//passwordInp.addTextChangedListener(new TextWatcher() {
+//@Override
+//public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//        }
+//
+//@Override
+//public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//        }
+//
+//@Override
+//public void afterTextChanged(Editable editable) {
+//
+//        }
+//        });
