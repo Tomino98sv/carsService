3af78121d2ade9af18e962ad04751480a3d42993
Tomas <tomas.pavlik@akademiasovy.sk>
2020-01-12 12:30:15 +0100

    added some funkcionalit

--- CarServiceApp/app/src/main/java/com/globalsovy/carserviceapp/ForgetPassword/NotRegisterEmail.java
+++ CarServiceApp/app/src/main/java/com/globalsovy/carserviceapp/ForgetPassword/NotRegisterEmail.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.globalsovy.carserviceapp.R;
+
 public class NotRegisterEmail extends AppCompatActivity {
 
     @Override
